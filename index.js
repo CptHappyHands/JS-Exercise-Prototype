c40bd13d4d8c0b2b70b69186b6d1456a065aa1d3
Andrew Cummings <andrew.amalee@gmail.com>
2021-06-16 10:14:47 -0700

    Finished all required tasks, moving to stretch

--- index.js
+++ index.js
@@ -39,13 +39,26 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-     
-  }
+ function Person(name, age) {
+     this.name = name;
+     this.age = age;
+     this.stomach = [];
  
- 
+  Person.prototype.eat = function(food) {
+    if(this.stomach.length<10){
+    this.stomach.push(food);
+    }
+  };
 
-  
+  Person.prototype.poop = function() {
+    this.stomach.splice(0, this.stomach.length);
+  }
+
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`
+  }
+
+}
   
   
   
@@ -63,9 +76,20 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+   this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
+
+  Car.prototype.fill = function(gallons){
+    return this.tank += gallons;
+  }
+
+  // Car.prototype.drive = 
+
+// 
   
   
   /*
@@ -75,18 +99,26 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
- 
   
+  function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age)
+    this.favoriteToy = favoriteToy;
+  }
+
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`
+  };
+ 
+
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. If in global scope, 'this' is everything in the window
+    2. Whenever you use a dot to call a function, whatever came before the dot is 'this'
+    3. If we use a constructor function, 'this' refers to the object that was created
+    4. Call and apply bind 'this' to whatever they called, which is used to override #3.
   */
   
   
